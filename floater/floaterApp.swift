ac9cd302d355fa8f694a54c5f3e30e841f80d185
Peter Chen <13093023233@163.com>
2023-09-04 20:37:17 +0800

    Initial Commit

---
+++ floater/floaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  floaterApp.swift
+//  floater
+//
+//  Created by Peter Chen on 2023/9/4.
+//
+
+import SwiftUI
+
+@main
+struct floaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
