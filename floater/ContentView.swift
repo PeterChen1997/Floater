f34d6e307e4a61774dde3c8cad27b0ade6f8e5c1
Peter Chen <13093023233@163.com>
2023-09-04 21:21:30 +0800

    feat: add code

--- floater/ContentView.swift
+++ floater/ContentView.swift
@@ -9,18 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            // side bar
+            if isMacOS() {
+                SideBar()
+            }
         }
-        .padding()
+        .frame(width: isMacOS() ? getRect().width/1.7 : nil, height: isMacOS() ? getRect().height - 180 : nil)
+        .background(Color("BG").ignoresSafeArea())
+        .preferredColorScheme(.light)
+        .buttonStyle(BorderlessButtonStyle())
+        .textFieldStyle(PlainTextFieldStyle())
+    }
+
+    @ViewBuilder
+    func SideBar() -> some View {
+        VStack {
+            Text("Floater")
+            .font(.title2)
+            .fontWeight(.semibold)
+            
+            AddButton()
+        }
+    }
+    
+    @ViewBuilder
+    func AddButton() -> some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "plus")
+                .font(.title2)
+                .foregroundColor(.white)
+                .padding(isMacOS() ? 12 : 15)
+                .background(Color.black)
+                .clipShape(Circle())
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func getRect() -> CGRect {
+        #if os(iOS)
+        return UIScreen.mani.bounds
+        #else
+        return NSScreen.main!.visibleFrame
+        #endif
+    }
+
+    func isMacOS() -> Bool {
+        #if os(iOS)
+        return false
+        #endif
+        return true
     }
 }
